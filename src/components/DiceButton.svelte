8a1acfbf2ca5b2cf0c5a4815dcdf669d1fc8f54b
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2022-07-30 18:29:57 -0400

    add data fetching logic

--- src/components/DiceButton.svelte
+++ src/components/DiceButton.svelte
@@ -1,4 +1,8 @@
-<button>
+<script>
+  export let onClick;
+</script>
+
+<button on:click={() => onClick()}>
   <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
@@ -16,5 +20,18 @@
     border: none;
     border-radius: 50%;
     background-color: var(--neon-green);
+    transition: box-shadow, transform 250ms ease;
+    will-change: contents;
+  }
+
+  button:hover {
+    box-shadow: 0px 0px 40px var(--neon-green);
+    cursor: pointer;
+    transform: scale(0.95);
+  }
+
+  button:hover > svg {
+    transition: all 300ms ease;
+    transform: rotate(45deg);
   }
 </style>
